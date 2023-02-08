14e902306a02b23bad5477b46e65d256bfe54ece
Apenas Esdras <93005998+ApenasEsdras@users.noreply.github.com>
2023-02-08 11:36:38 -0300

    01

---
+++ operadores/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Esdras So
+ *
+ */
+module operadores {
+}
---
+++ operadores/src/operadores/Operadores.java
@@ -0,0 +1,12 @@
+
+public class Operadores {
+	public static void main(string[]) {
+		
+		double nota1 = 9.0;
+		double nota2 = 10;
+		
+		double media =  (nota1 +  nota2)/2;
+		System.out.println(media);
+	}
+
+}
